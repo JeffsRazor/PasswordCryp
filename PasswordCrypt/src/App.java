d10bb983396f10998e15ff612ea85ebae5aafa9e
JeffsRazor <jeffthegrey8@gmail.com>
2020-12-06 22:41:07 -0500

    first commit

---
+++ PasswordCrypt/src/App.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+import org.mindrot.jbcrypt.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       Boolean looped = true;
+        do {
+        System.out.println("Enter password to crypt: ");
+        Scanner scan = new Scanner(System.in);
+        String pass = scan.nextLine();
+        System.out.println("Yoour pass: " + pass);
+        String hashed = BCrypt.hashpw(pass, BCrypt.gensalt(16));
+        System.out.println("Enter your password again to verify: ");
+        String check = scan.nextLine();
+        if(BCrypt.checkpw(check, hashed)){
+        System.out.println("Your hashed pass: " + hashed );
+        looped= false;
+        scan.close();
+        }
+        else {
+            System.out.println("Error: hashed password does not match");
+        }
+    }while(looped);
+    
+    }
+}
